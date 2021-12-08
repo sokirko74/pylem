532757beb14339fc033b32629a2c8724fb3e5d4c
U-DELL-7440\sokirko <sokirko@yandex.ru>
2021-12-08 13:38:05 +0300

    use . if path is empty

--- pylem/pylem_binary.py
+++ pylem/pylem_binary.py
@@ -3,7 +3,10 @@
     import sys, pkg_resources, importlib.util, os
     library_name = "pylem_binary"
     def find_compiled_library():
-        for i in os.listdir(os.path.dirname(__file__)):
+        f  =  os.path.dirname(__file__)
+        if  len(f) == 0:
+            f =  "."
+        for i in os.listdir(f):
             if i.startswith(library_name) and (i.endswith('.pyd') or i.endswith('.so')):
                 return i
         raise Exception ("no binary library found to load")
