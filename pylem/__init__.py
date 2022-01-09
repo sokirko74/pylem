89bf6867ead015d52cbe6b07a11e17974580dea0
sokirko74 <sokirko@yandex.ru>
2022-01-09 12:36:36 +0300

    add weights

--- pylem/__init__.py
+++ pylem/__init__.py
@@ -25,12 +25,15 @@ class MorphLanguage:
 
 
 class LemmaInfo:
-    def __init__(self, predicted, lemma = None, part_of_speech = None, morph_features=None, predicted_by=None):
+    def __init__(self, predicted, lemma=None, part_of_speech=None, morph_features=None, predicted_by=None,
+                 word_weight=None, homonym_weight=None):
         self.predicted = predicted
         self.lemma = lemma
         self.part_of_speech = part_of_speech
         self.morph_features = morph_features
         self.predicted_by = predicted_by
+        self.word_weight = word_weight
+        self.homonym_weight = homonym_weight
 
 
 class MorphanHolder:
@@ -60,7 +63,10 @@ class MorphanHolder:
                                   lemma=l['wordForm'],
                                   part_of_speech=pos,
                                   morph_features=grammems,
-                                  predicted_by=l.get('srcNorm'))
+                                  predicted_by=l.get('srcNorm'),
+                                  word_weight=l.get('wordWeight'),
+                                  homonym_weight=l.get('homonymWeight'),
+                                  )
             yield lemm_info
 
     def is_in_dictionary(self, word):
